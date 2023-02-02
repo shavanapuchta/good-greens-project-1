6a2a0db307756cb41243d3192c329a75b87b4b97
Geez-Us <noahsullivan382@gmail.com>
2023-02-02 15:05:25 -0800

    larger radius, better search result return

--- Skeleton-2.0.4/java.js
+++ Skeleton-2.0.4/java.js
@@ -64,7 +64,7 @@ function initMap(ev) {
   };
 
   service.nearbySearch(
-    { location: ev, radius: 2000, type: "restaurant", keyword: "Vegetarian"},
+    { location: ev, radius: 5000, type: "restaurant", keyword: "Vegetarian"},
     (results, status, pagination) => {
       if (status !== "OK" || !results) return;
 

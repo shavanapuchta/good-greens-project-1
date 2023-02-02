02574d70ca6aa7249f178465967ec0e705fd01bf
Geez-Us <noahsullivan382@gmail.com>
2023-02-02 11:25:06 -0800

    asynchronous remodel of initmap and openweather api

--- Skeleton-2.0.4/java.js
+++ Skeleton-2.0.4/java.js
@@ -5,12 +5,31 @@ var weatherKey = "4b68235b6d97901e4e6eb2b454a04dd0";
 var map;
 var service;
 var infowindow;
-var searchBtn = document.querySelector(".search-button")
+var searchBtn = document.querySelector(".search-button");
+document.querySelector("#googlesearch").value = "San Diego";
+searchCity();
 
-
-
-function searchCity(city) {
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey;
+var coord = {lat: 32.11, lng: -117.32}
+console.log(coord)
+var daygo = "San Diego"
+var cal = "California"
+//searchCity(daygo);
+searchBtn.addEventListener("click", function () {
+  //var cityName = document.querySelector("#googlesearch").value
+  //console.log(cityName)
+  var stateName = document.querySelector('select[name="state"]').value
+  console.log(stateName)
+  searchCity()
+  var coo = JSON.parse(localStorage.getItem("city"));
+  console.log(coo);
+  //var storedcity = localStorage.getItem("city");
+  //console.log(JSON.parse(storedcity));
+  
+})
+function searchCity() {
+  var cityName = document.querySelector("#googlesearch").value
+  console.log(cityName);
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + weatherKey;
   console.log(queryURL)
   fetch(queryURL).then(function (response) {
     console.log(response)
@@ -18,40 +37,31 @@ function searchCity(city) {
 
   })
     .then(function (data) {
+      
       console.log(data);
-
-      function initMap() {
-        const map = new google.maps.Map(document.getElementById("map"), {
-          center: { lat: data.coord.lat, lng: data.coord.lon },
-          zoom: 13,
-          mapId: "5e79efcdf99d1225",
-        });
-        const marker = new google.maps.Marker({
-          position: { lat: data.coord.lat, lng: data.coord.lon },
-          map: map,
-        })
-
-        const service = new google.maps.places.PlacesService(map)
-        // // we will want to find the restaurant and vegetarian in here 
-      }
-      window.initMap = initMap();
-    })
-
-}
-var daygo = "San Diego"
-var cal = "California"
-searchCity(daygo);
-
-
-searchBtn.addEventListener("click", function () {
-  var cityName = document.querySelector("#googlesearch").value
-  console.log(cityName)
-  var stateName = document.querySelector('select[name="state"]').value
-  console.log(stateName)
-  searchCity(cityName, stateName)
+      coord = {lat: data.coord.lat, lng: data.coord.lon};
+      console.log(coord)
+      localStorage.setItem("city", JSON.stringify(coord));
+      initMap(coord);
 })
+}
 
+function initMap(ev) {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: ev,
+    zoom: 13,
+    mapId: "5e79efcdf99d1225",
+  });
+  const marker = new google.maps.Marker({
+    position: ev,
+    map: map,
+  })
 
+  const service = new google.maps.places.PlacesService(map)
+  // // we will want to find the restaurant and vegetarian in here 
+}
+
+window.initMap = initMap();
 // Need to figure out what additional call we need to figure out to get to the restaurants for the location. 
 
 

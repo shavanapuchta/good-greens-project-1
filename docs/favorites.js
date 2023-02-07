431d91c9246dd23f152a95a2298a55d8830ffb84
TheRealGeneParmesan <Geneparmigiano@gmail.com>
2023-02-06 19:27:53 -0800

    linking the map functionality with favorites

--- docs/favorites.js
+++ docs/favorites.js
@@ -2,21 +2,65 @@
 
 // Above is the query URL to fetch a restaurant by placeID 
 
-var APIkey = "AIzaSyDbRLVtnxgOTPF4I23qMVJ908WFl6-TDOc"
-
+var APIkey = "AIzaSyDbRLVtnxgOTPF4I23qMVJ908WFl6-TDOc";
+var weatherKey = "4b68235b6d97901e4e6eb2b454a04dd0";
+var marker;
 // Grabs favorites array from local storage
 var favoritesList = JSON.parse(localStorage.getItem("favorites")) || [];
 
-// Create function that loops through favoriteList and fetches for each placeID  
+var coord = { lat: 32.7153, lng: -117.1573 }
+console.log(coord)
+
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: coord,
+        zoom: 13,
+        mapId: "5e79efcdf99d1225",
+    });
+}
+// Create function that loops through favoriteList and grabs each placeID  
 
 for (let i = 0; i < favoritesList.length; i++) {
     console.log(favoritesList[i]);
+
+    var request = {
+        placeId: [i],
+        fields: ["name", "formatted_address", "place_id", "geometry"],
+    };
+    console.log(request)
+}
+const infowindow = new google.maps.InfoWindow();
+const service = new google.maps.places.PlacesService(map);
+
+service.getDetails(request, (place, status) => {
+    if (
+        status === google.maps.places.PlacesServiceStatus.OK &&
+        place &&
+        place.geometry &&
+        place.geometry.location
+    ) {
+        const marker = new google.maps.Marker({
+            map,
+            position: place.geometry.location,
+        });
+    }
+})
+
+function createMarker(place) {
+    if (!place.geometry || !place.geometry.location) return;
+
+    marker = new google.maps.Marker({
+        map,
+        position: place.geometry.location,
+    });
+
 }
 
-window.onload = (event) => {
-    console.log('The page has fully loaded');
-    addFavorites();
-}
+
+window.initMap = initMap();
+
+
 
 function addFavorites() {
     console.log(favoritesList)
@@ -28,6 +72,14 @@ function addFavorites() {
 
     li.innerHTML = favoritesList + " did I do that?"
 }
+
+
+
+
+
+
+
+
 
 
 
@@ -50,4 +102,3 @@ function addFavorites() {
 //     place.formatted_address;
 // infowindow.open(map, marker);
 
-// window.initMap = initMap;
